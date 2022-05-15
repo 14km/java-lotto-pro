c57effb3848f335bee564b4c2bb9f18ad1f59aa7
MinTae Kim <45898974+14km@users.noreply.github.com>
2022-05-16 01:22:01 +0900

    Revert "feat: Lotto Result 기능 추가 및 테스트코드 추가"
    This reverts commit ce42105f4d4f17ec71c00103868c6ee02bcaf56d.

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import lotto.enums.LottoRank;
 
 public class LottoResult {
-    private static final double CRITERION = 1;
 
     public static double lottoGameEarningsRate(int money, LottoRanks lottoRanks) {
         return (double) totalPrizeMoney(lottoRanks) / money;
@@ -11,9 +10,5 @@ public class LottoResult {
 
     private static int totalPrizeMoney(LottoRanks lottoRanks) {
         return lottoRanks.getLottoRanks().stream().mapToInt(LottoRank::getPrizeMoney).sum();
-    }
-
-    public static boolean isCriterionRate(double earningsRate) {
-        return earningsRate > CRITERION;
     }
 }
